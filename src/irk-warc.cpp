eafc60176ad977dd5135a822b9a4503de9cbb7f4
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2018-03-10 20:12:15 -0500

    Migrate irk-warc to CLI11

--- src/irk-warc.cpp
+++ src/irk-warc.cpp
@@ -1,3 +1,4 @@
+#include <CLI/CLI.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/iostreams/filter/gzip.hpp>
 #include <boost/iostreams/filtering_stream.hpp>
@@ -34,77 +35,68 @@ void write_term(std::string&& term, SN_env* z, bool lowercase)
 
 int main(int argc, char** argv)
 {
-    irk::CmdLineProgram program("irk-warc");
-    program.flag("zip,z", "use zipped input files")
-        .flag("stem,s", "stem terms")
-        .flag("lowercase,l", "transform all characters to lower case")
-        .flag("help,h", "print help")
-        .flag("skip-header", "skip header defining column names")
-        .option<std::string>("fieldsep,f", "header field separator", "\t")
-        .arg<std::vector<std::string>>("input", "input WARC files", -1);
-    try {
-        if (!program.parse(argc, argv)) {
-            return 0;
-        }
-    } catch (irk::po::error& e) {
-        std::cout << e.what() << std::endl;
+    std::string field_separator = "\t";
+    std::vector<std::string> input_files;
+
+    CLI::App app{"irk-warc"};
+    app.add_flag("-z,--zip", "use zipped input files");
+    app.add_flag("-s,--stem", "stem terms");
+    app.add_flag("-l,--lowercase", "transform all characters to lower case");
+    app.add_flag("--skip-header", "skip header defining column names");
+    app.add_option("-f,--fieldsep",
+        field_separator,
+        "field separator in the output file",
+        true);
+    app.add_option("input", input_files, "input WARC files", false)
+        ->required()
+        ->check(CLI::ExistingFile);
+
+    CLI11_PARSE(app, argc, argv);
+
+    bool lowercase = app.count("--lowercase");
+
+    SN_env* z = nullptr;
+    if (app.count("--stem")) {
+        z = create_env();
     }
 
-    std::string field_separator = program.get<std::string>("fieldsep").value();
-    bool lowercase = program.defined("lowercase");
+    if (!app.count("--skip-header")) {
+        std::cout << "title\turl\tsize\tbody\n";
+    }
 
-    if (program.get<std::vector<std::string>>("input") == std::nullopt) {
-        std::cerr << "you must define at least one input file\n";
-        return 1;
-    } else {
-        SN_env * z = nullptr;
-        if (program.defined("stem")) {
-            z = create_env();
+    for (auto& input_file : input_files) {
+        std::ifstream fin(input_file);
+        boost::iostreams::filtering_istream in;
+        if (app.count("--zip")) {
+            in.push(boost::iostreams::gzip_decompressor());
         }
+        in.push(fin);
 
-        if (!program.defined("skip-header")) {
-            std::cout << "title\turl\tsize\tbody\n";
-        }
-
-        std::vector<std::string> input_files =
-            program.get<std::vector<std::string>>("input").value();
-
-        for (auto& input_file : input_files) {
-            std::ifstream fin(input_file);
-            boost::iostreams::filtering_istream in;
-            if (program.defined("zip")) {
-                in.push(boost::iostreams::gzip_decompressor());
-            }
-            in.push(fin);
-
-            irkit::io::warc_record record;
-            while (irkit::io::read_warc_record(in, record)) {
-                if (record.type() == "response") {
-                    std::cout << record.trecid() << field_separator;
-                    std::cout << record.url() << field_separator;
-                    std::cout << record.content_length() << field_separator;
-                    std::string content =
-                        irk::parsing::html::cleantext(record.content());
-                    std::regex term_pattern("(\\S+)");
-                    auto term_iter = std::sregex_iterator(
-                        content.begin(), content.end(), term_pattern);
-                    if (term_iter != std::sregex_iterator()) {
+        irkit::io::warc_record record;
+        while (irkit::io::read_warc_record(in, record)) {
+            if (record.type() == "response") {
+                std::cout << record.trecid() << field_separator;
+                std::cout << record.url() << field_separator;
+                std::cout << record.content_length() << field_separator;
+                std::string content =
+                    irk::parsing::html::cleantext(record.content());
+                std::regex term_pattern("(\\S+)");
+                auto term_iter = std::sregex_iterator(
+                    content.begin(), content.end(), term_pattern);
+                if (term_iter != std::sregex_iterator()) {
+                    write_term(term_iter->str(), z, lowercase);
+                    term_iter++;
+                    for (; term_iter != std::sregex_iterator(); term_iter++) {
+                        std::cout << " ";
                         write_term(term_iter->str(), z, lowercase);
-                        term_iter++;
-                        for (; term_iter != std::sregex_iterator();
-                             term_iter++) {
-                            std::cout << " ";
-                            write_term(term_iter->str(), z, lowercase);
-                        }
-                        std::cout << std::endl;
                     }
+                    std::cout << std::endl;
                 }
             }
         }
-        if (program.defined("stem")) {
-            close_env(z);
-        }
     }
-
+    if (app.count("--stem")) {
+        close_env(z);
+    }
 }
 
