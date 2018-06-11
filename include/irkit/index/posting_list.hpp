ff1b7bcc50fdabffabbc90487ed191b391dcc88f
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2018-06-11 12:24:04 -0400

    Implement general version of posting list view.

---
+++ include/irkit/index/posting_list.hpp
@@ -0,0 +1,131 @@
+// MIT License
+//
+// Copyright (c) 2018 Michal Siedlaczek
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+//! \file documentlist.hpp
+//! \author Michal Siedlaczek
+//! \copyright MIT License
+
+#pragma once
+
+#include <boost/iterator/iterator_facade.hpp>
+
+namespace irk {
+
+// TODO: Make variable numbers of payloads
+template<class DocumentListT, class PayloadListT>
+class posting_list_view {
+public:
+    using document_list_type = DocumentListT;
+    using payload_list_type = PayloadListT;
+    using document_type = typename document_list_type::value_type;
+    using payload_type = typename document_list_type::value_type;
+    using difference_type = long;
+    using document_iterator_t = typename document_list_type::iterator;
+    using payload_iterator_t = typename payload_list_type::iterator;
+
+    class posting_view {
+    public:
+        posting_view(document_iterator_t& document_iterator,
+            payload_iterator_t& payload_iterator)
+            : document_iterator_(document_iterator),
+              payload_iterator_(payload_iterator)
+        {}
+        document_type document() const { return *document_iterator_; }
+        payload_type payload() const { return *payload_iterator_; }
+
+    private:
+        document_iterator_t& document_iterator_;
+        payload_iterator_t& payload_iterator_;
+    };
+
+    class iterator : public boost::iterator_facade<iterator,
+                         posting_view,
+                         boost::forward_traversal_tag,
+                         posting_view> {
+    public:
+        iterator(document_iterator_t document_iterator,
+            payload_iterator_t payload_iterator)
+            : document_iterator_(document_iterator),
+              payload_iterator_(payload_iterator),
+              current_posting_(document_iterator_, payload_iterator_)
+        {}
+        iterator(const iterator& other)
+            : document_iterator_(other.document_iterator_),
+              payload_iterator_(other.payload_iterator_),
+              current_posting_(document_iterator_, payload_iterator_)
+        {}
+
+        iterator& moveto(document_type doc)
+        {
+            document_iterator_.moveto(doc);
+            payload_iterator_.align(document_iterator_);
+            return *this;
+        }
+
+        iterator nextgeq(document_type doc) const
+        {
+            iterator iter(*this);
+            iter.moveto(doc);
+            return iter;
+        }
+
+    private:
+        friend class boost::iterator_core_access;
+        void increment()
+        {
+            document_iterator_++;
+            payload_iterator_++;
+        }
+        void advance(difference_type n)
+        {
+            document_iterator_ += n;
+            payload_iterator_ += n;
+        }
+        bool equal(const iterator& other) const
+        {
+            return document_iterator_ == other.document_iterator_;
+        }
+
+        const posting_view& dereference() const { return current_posting_; }
+
+        document_iterator_t document_iterator_;
+        payload_iterator_t payload_iterator_;
+        posting_view current_posting_;
+    };
+    using const_iterator = iterator;
+
+    posting_list_view(document_list_type documents, payload_list_type payloads)
+        : documents_(documents), payloads_(payloads)
+    {}
+
+    iterator begin() const
+    { return iterator(documents_.begin(), payloads_.begin()); };
+    iterator end() const
+    { return iterator(documents_.end(), payloads_.end()); };
+    iterator lookup(document_type id) const { return begin().nextgeq(id); };
+
+private:
+    document_list_type documents_;
+    payload_list_type payloads_;
+};
+
+}  // namespace irk
---
+++ tests/unit/unit_test_posting_list.cpp
@@ -0,0 +1,96 @@
+// MIT License
+//
+// Copyright (c) 2018 Michal Siedlaczek
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+//! \file unit_test_posting_list.hpp
+//! \author Michal Siedlaczek
+//! \copyright MIT License
+
+#include <algorithm>
+#include <sstream>
+#include <vector>
+
+#include <gmock/gmock.h>
+#include <gsl/span>
+#include <gtest/gtest.h>
+
+#define private public
+#define protected public
+#include <irkit/index/vector_inverted_list.hpp>
+#include <irkit/index/posting_list.hpp>
+
+namespace {
+
+TEST(posting_list_view, forward_iterator)
+{
+    std::vector<long> documents = {0, 1, 4, 6, 9, 11, 30};
+    std::vector<double> payloads = {0, 1, 4, 6, 9, 11, 30};
+    irk::vector_document_list vdl(documents);
+    irk::vector_payload_list vpl(payloads);
+    vdl.block_size(3);
+    vpl.block_size(3);
+
+    irk::posting_list_view postings(vdl, vpl);
+    int idx = 0;
+    for (auto posting : postings) {
+        ASSERT_EQ(posting.document(), documents[idx]);
+        ASSERT_EQ(posting.payload(), payloads[idx]);
+        idx++;
+    }
+}
+
+template<class T>
+std::pair<long, double> pair_of(const T& posting)
+{
+    return std::make_pair(posting.document(), posting.payload());
+}
+
+TEST(posting_list_view, lookup)
+{
+    std::vector<long> documents = {0, 1, 4, 6, 9, 11, 30};
+    std::vector<double> payloads = {0, 1, 4, 6, 9, 11, 30};
+    irk::vector_document_list vdl(documents);
+    irk::vector_payload_list vpl(payloads);
+    vdl.block_size(3);
+    vpl.block_size(3);
+
+    irk::posting_list_view postings(vdl, vpl);
+    ASSERT_THAT(pair_of(*postings.lookup(1)), ::testing::Pair(1, 1));
+    ASSERT_THAT(pair_of(*postings.lookup(2)), ::testing::Pair(4, 4));
+    ASSERT_THAT(pair_of(*postings.lookup(3)), ::testing::Pair(4, 4));
+    ASSERT_THAT(pair_of(*postings.lookup(4)), ::testing::Pair(4, 4));
+    ASSERT_THAT(pair_of(*postings.lookup(5)), ::testing::Pair(6, 6));
+    ASSERT_THAT(pair_of(*postings.lookup(6)), ::testing::Pair(6, 6));
+    ASSERT_THAT(pair_of(*postings.lookup(7)), ::testing::Pair(9, 9));
+    ASSERT_THAT(pair_of(*postings.lookup(8)), ::testing::Pair(9, 9));
+    ASSERT_THAT(pair_of(*postings.lookup(9)), ::testing::Pair(9, 9));
+    ASSERT_THAT(pair_of(*postings.lookup(15)), ::testing::Pair(30, 30));
+    ASSERT_THAT(pair_of(*postings.lookup(30)), ::testing::Pair(30, 30));
+    ASSERT_EQ(postings.lookup(31), postings.end());
+}
+
+};  // namespace
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
