43a384c21db0e8ebdbd6e3d1abca60814889919e
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2018-06-11 09:36:27 -0400

    Remove ambiguous rvalue ref constructor

--- include/irkit/index/vector_inverted_list.hpp
+++ include/irkit/index/vector_inverted_list.hpp
@@ -195,9 +195,6 @@ public:
     vector_payload_list(std::vector<value_type> vec)
         : ids_(std::move(vec)), block_size_(ids_.size())
     {}
-    vector_payload_list(std::vector<value_type>&& vec)
-        : ids_(vec), block_size_(ids_.size())
-    {}
 
     size_type size() const { return ids_.size(); }
     size_type block_size() const { return block_size_; }
