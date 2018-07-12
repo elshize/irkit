7856e378804882d1ca43868364030f3b6432097a
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2018-07-12 09:24:46 -0400

    CI: remove .py extension

--- scripts/clang-tidy.sh
+++ scripts/clang-tidy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cmake -D CMAKE_EXPORT_COMPILE_COMMANDS=ON -D IRKit_BUILD_TEST=OFF ..
 python ../scripts/remove-submodules.py
-run-clang-tidy.py -header-filter=.*include/irkit/[^0-9]*hpp
+run-clang-tidy -header-filter=.*include/irkit/[^0-9]*hpp
