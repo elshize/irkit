722bf0fb5b706c0fb9a45dc8113d3e8687d18567
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2018-07-21 10:32:14 -0400

    chdir to build

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,19 +1,22 @@
 SUITE=$1
 
 if [ "$SUITE" == "unit" ]; then
-    cmake -D CMAKE_BUILD_TYPE=Release -D IRKit_BUILD_INTEGRATION_TEST:BOOL=OFF -D IRKit_BUILD_BENCHMARKS:BOOL=OFF .
+    cd build
+    cmake -D CMAKE_BUILD_TYPE=Release -D IRKit_BUILD_INTEGRATION_TEST:BOOL=OFF -D IRKit_BUILD_BENCHMARKS:BOOL=OFF ..
     cat CMakeFiles/CMakeError.log
     cat CMakeFiles/CMakeOutput.log
     make VERBOSE=1
     ctest -V
 elif [ "$SUITE" == "integration" ]; then
-    cmake -D CMAKE_BUILD_TYPE=Release -D IRKit_BUILD_INTEGRATION_TEST:BOOL=ON -D IRKit_BUILD_BENCHMARKS:BOOL=OFF .
+    cd build
+    cmake -D CMAKE_BUILD_TYPE=Release -D IRKit_BUILD_INTEGRATION_TEST:BOOL=ON -D IRKit_BUILD_BENCHMARKS:BOOL=OFF ..
     cat CMakeFiles/CMakeError.log
     cat CMakeFiles/CMakeOutput.log
     make VERBOSE=1
     ctest -V -R integration*
 elif [ "$SUITE" == "benchmarks" ]; then
-    cmake -D CMAKE_BUILD_TYPE=Release -D IRKit_BUILD_INTEGRATION_TEST:BOOL=OFF -D IRKit_BUILD_BENCHMARKS:BOOL=ON .
+    cd build
+    cmake -D CMAKE_BUILD_TYPE=Release -D IRKit_BUILD_INTEGRATION_TEST:BOOL=OFF -D IRKit_BUILD_BENCHMARKS:BOOL=ON ..
     cat CMakeFiles/CMakeError.log
     cat CMakeFiles/CMakeOutput.log
     make VERBOSE=1
