a0fa76949c0ed0a7d4cb7f8d96c1974bd5821c7c
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2018-07-21 10:12:13 -0400

    Switch to conan

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ class HelloConan(ConanFile):
     name = "irkit"
     version = "0.1"
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake_find_package", "cmake_paths"
+    generators = "cmake"
 
     def source(self):
         self.run("git clone https://github.com/elshize/irkit.git")
@@ -17,5 +17,21 @@ class HelloConan(ConanFile):
         cmake.test()
 
     def requirements(self):
-        self.requires("zlib/1.2.11@conan/stable")
+        self.requires("streamvbyte/master@elshize/testing")
         self.requires("gumbo-parser/1.0@elshize/stable")
+        self.requires("rangev3/master@elshize/testing")
+        self.requires("nlohmann_json/3.1.2@elshize/stable")
+        self.requires("rax/master@elshize/testing")
+
+        self.requires("boost/1.67.0@conan/stable")
+        self.requires("zlib/1.2.11@conan/stable")
+        self.requires("gtest/1.8.0@conan/stable")
+
+        self.requires("CLI11/1.6.0@cliutils/stable")
+        self.requires("gsl_microsoft/1.0.0@bincrafters/stable")
+        self.requires("debug_assert/1.3@Manu343726/testing")
+        self.requires("type_safe/0.1@Manu343726/testing")
+        self.requires("gsl_microsoft/1.0.0@bincrafters/stable")
+
+    def configure(self):
+        self.options["boost"].shared = True
