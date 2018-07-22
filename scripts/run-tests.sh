481101cb5a06dfd41ff3fa2489e19d161dee568c
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2018-07-22 12:32:12 -0400

    Use system boost for all CI

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,21 +3,15 @@ SUITE=$1
 if [ "$SUITE" == "unit" ]; then
     cd build
     cmake -D CMAKE_BUILD_TYPE=Release -D IRKit_USE_SYSTEM_BOOST=ON -D IRKit_BUILD_INTEGRATION_TEST=OFF -D IRKit_BUILD_BENCHMARKS=OFF ..
-    cat CMakeFiles/CMakeError.log
-    cat CMakeFiles/CMakeOutput.log
     make VERBOSE=1
     ctest -V
 elif [ "$SUITE" == "integration" ]; then
     cd build
-    cmake -D CMAKE_BUILD_TYPE=Release -D IRKit_BUILD_INTEGRATION_TEST=ON -D IRKit_BUILD_BENCHMARKS=OFF ..
-    cat CMakeFiles/CMakeError.log
-    cat CMakeFiles/CMakeOutput.log
+    cmake -D CMAKE_BUILD_TYPE=Release -D IRKit_USE_SYSTEM_BOOST=ON -D IRKit_BUILD_INTEGRATION_TEST=ON -D IRKit_BUILD_BENCHMARKS=OFF ..
     make VERBOSE=1
     ctest -V -R integration*
 elif [ "$SUITE" == "benchmarks" ]; then
     cd build
-    cmake -D CMAKE_BUILD_TYPE=Release -D IRKit_BUILD_INTEGRATION_TEST=OFF -D IRKit_BUILD_BENCHMARKS=ON ..
-    cat CMakeFiles/CMakeError.log
-    cat CMakeFiles/CMakeOutput.log
+    cmake -D CMAKE_BUILD_TYPE=Release -D IRKit_USE_SYSTEM_BOOST=ON -D IRKit_BUILD_INTEGRATION_TEST=OFF -D IRKit_BUILD_BENCHMARKS=ON ..
     make VERBOSE=1
 fi
