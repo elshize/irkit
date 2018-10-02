eec10bc9efe7fe89ffed88500d269aa06b423b72
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2018-10-02 15:12:45 -0400

    Fix TAAT benchmark compilation error

--- benchmarks/bench_taat.cpp
+++ benchmarks/bench_taat.cpp
@@ -95,7 +95,8 @@ int main(int argc, char** argv)
 
     CLI11_PARSE(app, argc, argv);
     boost::filesystem::path dir(index_dir);
-    auto data = irk::inverted_index_disk_data_source::from(dir, {"bm25"});
+    auto data =
+        irk::inverted_index_disk_data_source::from(dir, {"bm25"}).value();
     irk::inverted_index_view index(&data);
 
     nanoseconds total(0);
