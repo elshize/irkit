fd7c0dfd143bfc1c57f9e09ec50c89cd56d5c6b6
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2018-11-03 12:34:25 -0400

    Conan: get entire boost

--- conanfile.py
+++ conanfile.py
@@ -11,29 +11,7 @@ class IRKConan(ConanFile):
     generators = "cmake", "ycm", "cmake_paths"
     options = {"use_system_boost": [True, False], "no_sanitizers": [True, False]}
     default_options = ("use_system_boost=False",
-                       "no_sanitizers=False",
-                       "boost:without_math=True",
-                       "boost:without_wave=True",
-                       "boost:without_container=True",
-                       "boost:without_exception=True",
-                       "boost:without_graph=True",
-                       "boost:without_locale=True",
-                       "boost:without_log=True",
-                       "boost:without_program_options=True",
-                       "boost:without_random=True",
-                       "boost:without_mpi=True",
-                       "boost:without_serialization=True",
-                       "boost:without_signals=True",
-                       "boost:without_coroutine=True",
-                       "boost:without_fiber=True",
-                       "boost:without_timer=True",
-                       "boost:without_graph_parallel=True",
-                       "boost:without_python=True",
-                       "boost:without_stacktrace=True",
-                       "boost:without_system=True",
-                       "boost:without_test=True",
-                       "boost:without_thread=True",
-                       "boost:without_type_erasure=True")
+                       "no_sanitizers=False")
     exports_sources = ("LICENSE", "README.md", "include/*", "src/*",
                        "cmake/*", "CMakeLists.txt", "tests/*", "benchmarks/*",
                        "scripts/*")
