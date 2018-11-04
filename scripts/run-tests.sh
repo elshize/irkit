daebc5dc9a0eb36cedafe8e01607b842b25bc87c
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2018-11-04 07:49:11 -0500

    CI: Use system boost

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,6 +3,7 @@ SUITE=$1
 if [ "$SUITE" == "unit" ]; then
     cd build
     cmake -D CMAKE_BUILD_TYPE=Release \
+        -D IRKit_USE_SYSTEM_BOOST=ON \
         -D IRKit_BUILD_INTEGRATION_TEST=OFF \
         -D IRKit_BUILD_BENCHMARKS=OFF \
         -D IRKit_NO_SANITIZERS=ON \
@@ -12,6 +13,7 @@ if [ "$SUITE" == "unit" ]; then
 elif [ "$SUITE" == "integration" ]; then
     cd build
     cmake -D CMAKE_BUILD_TYPE=Release \
+        -D IRKit_USE_SYSTEM_BOOST=ON \
         -D IRKit_BUILD_INTEGRATION_TEST=ON \
         -D IRKit_BUILD_BENCHMARKS=OFF \
         -D IRKit_NO_SANITIZERS=ON \
@@ -21,6 +23,7 @@ elif [ "$SUITE" == "integration" ]; then
 elif [ "$SUITE" == "benchmarks" ]; then
     cd build
     cmake -D CMAKE_BUILD_TYPE=Release \
+        -D IRKit_USE_SYSTEM_BOOST=ON \
         -D IRKit_BUILD_INTEGRATION_TEST=OFF \
         -D IRKit_BUILD_BENCHMARKS=ON \
         -D IRKit_NO_SANITIZERS=ON \
