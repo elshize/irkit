d8494094bd7390147b2363cbb0a97f1f6d3f345f
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2018-11-11 18:22:00 -0500

    Update posting list unit test

--- tests/unit/unit_test_posting_list.cpp
+++ tests/unit/unit_test_posting_list.cpp
@@ -41,12 +41,172 @@
 
 namespace {
 
+namespace tmp {
+
+    template<typename T>
+    using OrderFn = std::function<bool(const T&, const T&)>;
+
+    template<typename Iterator>
+    using range_vector =
+        std::vector<std::pair<std::ptrdiff_t, irk::moving_range<Iterator>>>;
+
+    template<typename T, typename Iterator>
+    class PositionedUnionIterator
+        : public boost::iterator_facade<
+              PositionedUnionIterator<T, Iterator>,
+              std::pair<std::ptrdiff_t, T>,
+              boost::forward_traversal_tag,
+              std::pair<std::ptrdiff_t, const T&>> {
+    public:
+        using OrderFn = irk::OrderFn<irk::moving_range<Iterator>>;
+        PositionedUnionIterator(
+            range_vector<Iterator> ranges,
+            OrderFn order,
+            size_t pos,
+            size_t length)
+            : ranges_(std::move(ranges)),
+              order_(std::move(order)),
+              pos_(pos),
+              length_(length)
+        {
+            std::sort(
+                ranges_.begin(),
+                ranges_.end(),
+                [this](const auto& lhs, const auto& rhs) {
+                    return order_(lhs.second, rhs.second);
+                });
+        }
+
+    private:
+        friend class boost::iterator_core_access;
+        void increment()
+        {
+            if (pos_ == length_) {
+                return;
+            }
+            ranges_.front().second.advance();
+            for (auto it = std::next(ranges_.begin()); it != ranges_.end();
+                 ++it) {
+                auto prev = std::prev(it);
+                if (order_(prev->second, it->second)) {
+                    break;
+                }
+                std::iter_swap(prev, it);
+            }
+            pos_ += 1;
+        }
+        bool equal(const PositionedUnionIterator& other) const
+        {
+            return pos_ == other.pos_;
+        }
+        std::pair<std::ptrdiff_t, const T&> dereference() const
+        {
+            return std::make_pair(
+                ranges_.front().first, ranges_.front().second.front());
+        }
+
+        range_vector<Iterator> ranges_;
+        OrderFn order_;
+        size_t pos_;
+        size_t length_;
+    };
+
+    template<typename T, typename PostingList>
+    class PositionedUnion {
+        using list_iterator = typename PostingList::iterator;
+
+        range_vector<list_iterator> retrieve_ranges(
+            std::function<irk::moving_range<list_iterator>(const PostingList&)>
+                fun) const
+        {
+            range_vector<list_iterator> ranges;
+            std::ptrdiff_t id{0};
+            std::transform(
+                lists_.begin(),
+                lists_.end(),
+                std::back_inserter(ranges),
+                [&fun, &id](const auto& list) {
+                    return std::make_pair(id++, fun(list));
+                });
+            return ranges;
+        }
+
+    public:
+        PositionedUnion(
+            std::vector<PostingList> lists,
+            irk::OrderFn<irk::moving_range<list_iterator>> order)
+            : lists_(std::move(lists)), order_(std::move(order))
+        {
+            length_ = std::accumulate(
+                lists_.begin(),
+                lists_.end(),
+                0,
+                [](const auto acc, const auto& list) {
+                    return acc + list.size();
+                });
+        }
+        auto begin() const
+        {
+            return PositionedUnionIterator<T, list_iterator>(
+                retrieve_ranges([](const auto& list) {
+                    return irk::moving_range(list.begin(), list.end());
+                }),
+                order_,
+                0,
+                length_);
+        }
+        auto end() const
+        {
+            return PositionedUnionIterator<T, list_iterator>(
+                retrieve_ranges([](const auto& list) {
+                    return irk::moving_range(list.end(), list.end());
+                }),
+                order_,
+                length_,
+                length_);
+        }
+        auto size() const { return length_; }
+
+    private:
+        std::vector<PostingList> lists_;
+        irk::OrderFn<irk::moving_range<list_iterator>> order_;
+        size_t length_;
+    };
+
+    template<typename Iter>
+    auto merge_positioned(Iter first_posting_list, Iter last_posting_list)
+    {
+        using list_type = std::remove_const_t<
+            std::remove_reference_t<decltype(*first_posting_list)>>;
+        using list_iterator = typename list_type::iterator;
+        return PositionedUnion<typename list_type::posting_view, list_type>(
+            std::vector<list_type>(first_posting_list, last_posting_list),
+            [](const irk::moving_range<list_iterator>& lhs,
+               const irk::moving_range<list_iterator>& rhs) {
+                if (rhs.empty()) {
+                    return true;
+                }
+                if (lhs.empty()) {
+                    return false;
+                }
+                return lhs.front().document() < rhs.front().document();
+            });
+    }
+
+    template<typename Range>
+    auto merge_positioned(const Range& posting_lists)
+    {
+        return merge_positioned(posting_lists.begin(), posting_lists.end());
+    }
+
+}  // namespace tmp
+
 TEST(posting_list_view, forward_iterator)
 {
     std::vector<long> documents = {0, 1, 4, 6, 9, 11, 30};
     std::vector<double> payloads = {0, 1, 4, 6, 9, 11, 30};
-    irk::vector_document_list vdl(documents);
-    irk::vector_payload_list vpl(payloads);
+    irk::vector_document_list vdl(0, documents);
+    irk::vector_payload_list vpl(0, payloads);
     vdl.block_size(3);
     vpl.block_size(3);
 
@@ -71,8 +231,8 @@ TEST(posting_list_view, lookup)
 {
     std::vector<long> documents = {0, 1, 4, 6, 9, 11, 30};
     std::vector<double> payloads = {0, 1, 4, 6, 9, 11, 30.1};
-    irk::vector_document_list vdl(documents);
-    irk::vector_payload_list vpl(payloads);
+    irk::vector_document_list vdl(0, documents);
+    irk::vector_payload_list vpl(0, payloads);
     vdl.block_size(3);
     vpl.block_size(3);
 
@@ -106,14 +266,14 @@ TEST(posting_list_view, union_view)
         irk::vector_payload_list<int>>>
         posting_lists;
     posting_lists.emplace_back(
-        irk::vector_document_list(std::vector<int>{0, 1, 4}),
-        irk::vector_payload_list(std::vector<int>{0, 0, 0}));
+        irk::vector_document_list(0, std::vector<int>{0, 1, 4}),
+        irk::vector_payload_list(0, std::vector<int>{0, 0, 0}));
     posting_lists.emplace_back(
-        irk::vector_document_list(std::vector<int>{0, 2, 4}),
-        irk::vector_payload_list(std::vector<int>{1, 1, 1}));
+        irk::vector_document_list(0, std::vector<int>{0, 2, 4}),
+        irk::vector_payload_list(0, std::vector<int>{1, 1, 1}));
     posting_lists.emplace_back(
-        irk::vector_document_list(std::vector<int>{1, 2, 4}),
-        irk::vector_payload_list(std::vector<int>{2, 2, 2}));
+        irk::vector_document_list(0, std::vector<int>{1, 2, 4}),
+        irk::vector_payload_list(0, std::vector<int>{2, 2, 2}));
 
     auto postings = irk::merge(posting_lists);
     std::vector<int> docs_only(postings.size());
@@ -146,8 +306,8 @@ TEST(scored_posting_list_view, forward_iterator)
 {
     std::vector<long> documents = {0, 1, 4, 6, 9, 11, 30};
     std::vector<double> payloads = {0, 1, 4, 6, 9, 11, 30};
-    irk::vector_document_list vdl(documents);
-    irk::vector_payload_list vpl(payloads);
+    irk::vector_document_list vdl(0, documents);
+    irk::vector_payload_list vpl(0, payloads);
     vdl.block_size(3);
     vpl.block_size(3);
 
@@ -163,6 +323,31 @@ TEST(scored_posting_list_view, forward_iterator)
         ASSERT_EQ(posting.payload(), payloads[idx] + 1);
         idx++;
     }
+}
+
+TEST(positioned_merge, iterator)
+{
+    std::vector<std::pair<std::vector<int>, std::vector<double>>> lists{
+        {{0, 1, 4, 6, 9, 11, 30}, {0, 1, 4, 6, 9, 11, 30}},
+        {{0, 1, 6, 9, 20, 30}, {0, 1, 6, 9, 20, 30}}};
+    std::vector<irk::posting_list_view<std::vector<int>, std::vector<double>>>
+        postings = {{lists[0].first, lists[0].second},
+                    {lists[1].first, lists[1].second}};
+    std::vector<int> expected_docs = {0, 0, 1, 1, 4, 6, 6, 9, 9, 11, 20, 30, 30};
+    std::vector<std::pair<std::ptrdiff_t, double>> expected_pairs = {
+        {0, 0}, {1, 0}, {0, 1}, {1, 1}, {0, 4}, {0, 6}, {1, 6}, {0, 9}, {1, 9},
+        {0, 11}, {1, 20}, {0, 30}, {1, 30}};
+
+    auto merged = tmp::merge_positioned(postings);
+
+    std::vector<int> docs;
+    std::vector<std::pair<std::ptrdiff_t, double>> pairs;
+    auto iter = merged.begin();
+    for (auto [list_id, posting] : merged) {
+        docs.push_back(posting.document());
+        pairs.push_back(std::make_pair(list_id, posting.payload()));
+    }
+    ASSERT_THAT(docs, ::testing::ElementsAreArray(expected_docs));
 }
 
 }  // namespace
